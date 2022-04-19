a18c521c9481a234356f2be70a65f47fee8f22c5
Drkbai <87938773+Drkbai@users.noreply.github.com>
2022-04-19 19:58:07 +0800

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        $faker = Faker::create();
+
+        foreach (range(1, 500) as $index) {
+                        
+            DB::table('backgroundImages')->insert([
+                'imgTitle' => Str::random(10),
+                'imgSrc' => "https://source.unsplash.com/random",
+            ]);
+        }
+    }
+}
